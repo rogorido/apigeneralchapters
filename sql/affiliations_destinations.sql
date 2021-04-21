5e2ca2eecc4501ebb06f29ed34c9daa4df62f23c
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2021-04-21 19:51:10 +0200

    Afiliaciones SQL

---
+++ sql/affiliations_destinations.sql
@@ -0,0 +1,9 @@
+
+SELECT h.name AS casa_destino,
+       p.name AS provincia,
+       COUNT(*) AS total
+FROM  houses h
+LEFT JOIN provinces P USING (province_id)
+LEFT JOIN resolutions_details r ON (details->>'casa_destino')::INT =h.house_id
+where r.details ? 'afiliacion'
+GROUP BY casa_destino, p.name;
